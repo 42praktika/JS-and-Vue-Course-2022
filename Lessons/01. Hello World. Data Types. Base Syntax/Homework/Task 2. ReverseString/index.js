8dcce40e59c712ea2130891433b8fa9338a32b89
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-17 19:43:42 +0300

    Homework 1, task 2

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
@@ -1,7 +1,12 @@
 const reverseString = (str) => {
-    // Начало
-
-    // Конец
+    let answer=""
+    if (str.length<=1) {
+        return str;
+    }
+    for (let i=str.length-1;i>=0;i--) {
+        answer+=str[i];
+    }
+    return answer
 };
 
 export default reverseString;
