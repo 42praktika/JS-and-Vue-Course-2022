b9ffd30e50532a371985e52bbf6eb6a2b9581f0f
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-17 19:23:32 +0300

    Homework 1, task 3

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
@@ -1,7 +1,13 @@
 const sumSquareDifference = (num) => {
-    // Начало
-    
-    // Конец
+    let firstsum=0;
+    let secondesum=0;
+    for (let i=1;i<=num;i++) {
+        firstsum+=i**2;
+    }
+    for (let j=1;j<=num;j++) {
+        secondesum+=j;
+    }
+    return secondesum**2-firstsum;
 };
 
 export default sumSquareDifference;
