57568fb30e5e9c1b03d15c6b869284f005c04acb
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-17 10:54:42 +0300

    Classwork 1, task 3

--- Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 3. IsTicketHappy/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 3. IsTicketHappy/index.js
@@ -1,7 +1,35 @@
 const isTicketHappy = (num) => {
-    // Начало
+    let Before=num.length;
+    let newNum = Number(num);
+    let massiv=Array.from(String(newNum),Number);
+    let After=massiv.length;
+    let need=Before-After;
+    for (var k=0;k<need;k++) {
+        massiv.unshift(0)
+    }
+    let first=0;
+    let second=0;
+    for (var i=0;i<massiv.length/2;i++) {
+        first+=massiv[i];
+    }
+    for (var j=massiv.length/2;j<massiv.length;j++) {
+        second+=massiv[j];
+    }
+    return first===second
 
-    // Конец
+
 };
+isTicketHappy('211003')
+isTicketHappy('1552');
+isTicketHappy('044503');
+isTicketHappy('00');
+isTicketHappy('060006');
+isTicketHappy('123321');
+isTicketHappy('341800');
+isTicketHappy('812146');
+isTicketHappy('000001');
+isTicketHappy('123567');
+isTicketHappy('213612');
 
 export default isTicketHappy;
+
