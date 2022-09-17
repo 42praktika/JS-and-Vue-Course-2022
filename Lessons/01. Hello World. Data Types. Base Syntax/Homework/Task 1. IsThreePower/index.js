d7613cfebed1765f17a430c17e19c066843b08a4
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-17 19:15:42 +0300

    Homework 1, task 1

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
@@ -1,7 +1,19 @@
 const isThreePower = (num) => {
-    // Начало
-
-    // Конец
+    while (num>1) {
+        if (num===0) {
+            return false;
+        }
+        else {
+            if (num%3===0) {
+                num=num/3;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    return num===1;
 };
+
 
 export default isThreePower;
