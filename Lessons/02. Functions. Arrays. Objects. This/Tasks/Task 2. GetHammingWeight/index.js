91e3ad7ee51857d9d77f491ebeb0ff633649c362
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-17 18:06:44 +0300

    Classwork 2, task 2

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 2. GetHammingWeight/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 2. GetHammingWeight/index.js
@@ -1,7 +1,17 @@
-const getHammingWeight = (num) => {
-    // Начало
 
-    // Конец
+const getHammingWeight = (num) => {
+    let count=0;
+    let big=num.toString(2);
+    let intbig=Number(big);
+    let massiv=Array.from(String(intbig),Number);
+    for (let i=0;i<massiv.length;i++) {
+        count+=massiv[i];
+    }
+    return count;
+
 };
+getHammingWeight(0);
+getHammingWeight(4);
+getHammingWeight(101);
 
 export default getHammingWeight;
