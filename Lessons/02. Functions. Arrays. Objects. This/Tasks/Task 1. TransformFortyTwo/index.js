39838548b51b24347bbc5f32351c1e7636b7c99b
Hehpheh <102379707+Hehpheh@users.noreply.github.com>
2022-09-18 16:13:39 +0300

    added task 02.c.1

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 1. TransformFortyTwo/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 1. TransformFortyTwo/index.js
@@ -1,5 +1,13 @@
 const transformFortyTwo = (numbers) => {
     // Начало
+  for(let i=0; i<numbers.length; i++){
+   
+    if(numbers[i]%42==0){
+      numbers.toString();
+        numbers[i]=`forty two!`;
+    }
+ }
+   return numbers;
     
     // Конец
 };
