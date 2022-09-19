bf3b38ad211c0cfd1fd2d9f2e7dd38c5cd38bbfe
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-19 16:38:37 +0300

    Classwork 3, task 3

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 3. GetPyramidVolume/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 3. GetPyramidVolume/index.js
@@ -1,7 +1,14 @@
+
 class Pyramid {
-    // Начало
-
-    // Конец
+    constructor(height,UpSquare,LowerSquare) {
+        this.height=height;
+        this.UpSquare=UpSquare;
+        this.LowerSquare=LowerSquare;
+    }
+    getVolume() {
+        return (Math.trunc((1/3)*this.height*(this.UpSquare+this.LowerSquare+(Math.sqrt(this.UpSquare)*Math.sqrt(this.LowerSquare)))*100)/100)
+    }
 }
+export default Pyramid
 
-export default Pyramid;
+
