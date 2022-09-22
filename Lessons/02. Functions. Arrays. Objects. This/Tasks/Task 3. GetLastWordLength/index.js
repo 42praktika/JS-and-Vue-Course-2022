48cdf596f1efe1622883d12acb08c2beaea614ea
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-22 09:01:29 +0300

    Classwork 2, task 3

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 3. GetLastWordLength/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 3. GetLastWordLength/index.js
@@ -1,7 +1,15 @@
 const getLastWordLength = (str) => {
-    // Начало
-
-    // Конец
+    let newStr=str.trim();
+    let answer=0;
+    for (let i=newStr.length-1;i>=0;i--) {
+        if (newStr[i]!==String.fromCharCode(32)) {
+            answer++;
+        }
+        else{
+            break;
+        }
+    }
+    return answer;
 };
 
 export default getLastWordLength;
