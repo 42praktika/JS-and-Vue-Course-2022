e7f0e56661bc581ab415c11ae29c64a9a2eb4dcb
AskarSaliakhov <90134116+AskarSaliakhov@users.noreply.github.com>
2022-09-22 19:01:58 +0300

    Update index.js

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
@@ -1,10 +1,7 @@
 const moreFunction = (number) => {
     return function newFunction(array) {
-        for (let i = 0; i < array.length; i++) {
-            array[i] = array[i] / number
-        }
-        return array;
+        let answer;
+        return answer=array.map(array=>array/number)
     }
 }
-
 export default moreFunction;
