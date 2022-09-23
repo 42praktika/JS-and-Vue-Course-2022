50b6c5ebc882b9ed4849a1380b41b07207dad880
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-24 00:41:02 +0300

    Classwork 2 , task 5

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 5. GetBudget/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 5. GetBudget/index.js
@@ -1,7 +1,8 @@
 const getBudget = (people) => {
-    // Начало
-
-    // Конец
-};
-
+    let allBudget = 0
+    for (let person = 0; person < people.length; person++) {
+        allBudget += people[person].budget
+    }
+    return allBudget
+}
 export default getBudget;
