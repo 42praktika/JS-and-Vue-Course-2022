89502d15d0feca87179a23aa598f01eed311b2e2
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-24 01:23:00 +0300

    Classwork 2,task 1

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 1. TransformFortyTwo/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 1. TransformFortyTwo/index.js
@@ -1,7 +1,12 @@
 const transformFortyTwo = (numbers) => {
-    // Начало
-
-    // Конец
-};
+    let array = numbers.map((num) => {
+        if (num % 42 !== 0) {
+            return num;
+        } else {
+            return "forty two!"
+        }
+    })
+    return array;
+}
 
 export default transformFortyTwo;
