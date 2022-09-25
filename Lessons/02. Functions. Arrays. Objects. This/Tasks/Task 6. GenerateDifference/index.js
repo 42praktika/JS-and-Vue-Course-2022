8b4878b99c31c3fc8d0295653f920d5b016f599d
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-25 20:13:14 +0300

    Classwork 2, task 6

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 6. GenerateDifference/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 6. GenerateDifference/index.js
@@ -1,13 +1,38 @@
-// Возвращает копию элемента
-const getCopy = (el) => JSON.parse(JSON.stringify(el));
-
-// Возвращает массив без дубликатов
-const onlyUnique = (array) => [ ...new Set(array) ];
-
 const generateDifference = (objOne, objTwo) => {
-    // Начало
+    let keys1 = Object.keys(objOne)
+    let values1 = Object.values(objOne)
+    let keys2 = Object.keys(objTwo)
+    let values2 = Object.values(objTwo)
+    let map = new Map();
+    for (let i = 0; i < keys1.length; i++) {
+        let flag = false;
+        for (let j = 0; j < keys2.length; j++) {
+            if (keys2[j] === keys1[i]) {
+                flag = true
+                if (values2[j] === values1[i]) {
+                    map.set(keys1[i], "unchanged")
+                } else {
+                    map.set(keys1[i], "changed")
+                }
+            }
+        }
+        if (!flag) {
+            map.set(keys1[i], "deleted")
+        }
+    }
+    for (let j = 0; j < keys2.length; j++) {
+        let count = 0;
+        for (let i = 0; i < keys1.length; i++) {
+            if (keys2[j] === keys1[i]) {
+                count++;
+            }
+        }
+        if (count === 0) {
+            map.set(keys2[j], "added")
 
-    // Конец
+        }
+    }
+    return Object.fromEntries(map)
 };
 
 export default generateDifference;
