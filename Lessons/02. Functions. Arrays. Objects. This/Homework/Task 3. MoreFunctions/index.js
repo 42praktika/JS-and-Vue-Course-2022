3ef9386367a49952676727b3e17011593f964f40
Hehpheh <102379707+Hehpheh@users.noreply.github.com>
2022-09-25 14:55:38 +0300

    added task 02.h.3

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
@@ -1,6 +1,12 @@
 const moreFunction = (number) => {
     // Начало
-
+  const newArray = (array) => {
+    for(let i = 0; i < array.length; i++){
+         array[i] = array[i] / number;
+    }
+    return array;
+  }
+  return newArray;
     // Конец
 };
 
