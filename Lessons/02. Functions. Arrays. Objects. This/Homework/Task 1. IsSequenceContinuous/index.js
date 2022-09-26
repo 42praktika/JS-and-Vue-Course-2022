f0ad53a8211b18dcbf3980d3a907c9ff37251726
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-09-26 23:03:16 +0300

    Update index.js

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 1. IsSequenceContinuous/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 1. IsSequenceContinuous/index.js
@@ -1,6 +1,17 @@
 const isSequenceContinuous = (numbers) => {
     // Начало
-
+    let count = 0
+    for (let i = 0; i <= numbers.length - 1; i++) {
+        if (numbers[i+1] - numbers[i] === 1) {
+            count = count + 1
+        }
+    }
+    if (count === numbers.length - 1 && numbers.length > 1) {
+        return true
+    }
+    else {
+        return false
+    }
     // Конец
 };
 
