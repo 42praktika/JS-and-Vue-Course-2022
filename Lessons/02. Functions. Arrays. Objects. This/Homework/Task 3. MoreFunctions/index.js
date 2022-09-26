f85e2afe4aebb70976cf759166dd931cabe5dd38
Safiullin.Ayaz <tony.holl2609@gmail.com>
2022-09-26 15:21:47 +0300

    [UPD] Lesson 2 CW/HW

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
@@ -1,5 +1,10 @@
 const moreFunction = (number) => {
-
+    return (numbers) => {
+        for (let i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[i] / number;
+        }
+        return numbers;
+    };
 };
 
 export default moreFunction;
