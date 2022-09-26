166b90ae79dc01ba81d3b89b6296b2e0f7b68a43
DariaGazkaeva <d.gazkaeva@gmail.com>
2022-09-26 14:03:38 +0100

    did lesson3.task1 but it does not work

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 1. ReorderDigits/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 1. ReorderDigits/index.js
@@ -1,5 +1,22 @@
 const reorderDigits = () => {
 
+    let arr = Array.prototype.slice.call(arguments);
+
+    const bubbleSort = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr.length() - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let x = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = x;
+                }
+            }
+        }
+        return arr;
+    }
+
+    if (arguments[0] === 'asc') return bubbleSort(arr.slice(1, arr.length));
+    if (arguments[0] === 'desc') return bubbleSort(arr.slice(1, arr.length)).reverse();
 };
 
 export default reorderDigits;
