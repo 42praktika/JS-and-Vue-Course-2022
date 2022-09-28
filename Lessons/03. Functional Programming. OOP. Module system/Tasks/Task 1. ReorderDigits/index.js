67009d62f434bbb52667b0a977a50184e3a7b8d4
AskarSaliahov <askarsaliahov@gmail.com>
2022-09-28 22:19:47 +0300

    Classwork 3 , task 1

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 1. ReorderDigits/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 1. ReorderDigits/index.js
@@ -1,7 +1,12 @@
-const reorderDigits = (type, ...numbers) => {
-    // Начало
 
-    // Конец
+const reorderDigits = (type, ...numbers) => {
+    if (type==="asc") {
+        return numbers.sort(((a,b)=>a-b));
+    }
+    else {
+        return numbers.sort((a,b)=>b-a)
+    }
 };
+
 
 export default reorderDigits;
