9c2afcd856fcc04a11994f23ae740e5fbe4b48ef
DariaGazkaeva <d.gazkaeva@gmail.com>
2022-10-02 23:07:54 +0100

    finished lesson3.task4

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 4. CityGame/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 4. CityGame/index.js
@@ -1,7 +1,34 @@
 class CityGame {
-    // Начало
+    constructor(name1, name2) {
+        this.arr = [];
+        this.name1 = name1;
+        this.name2 = name2;
+    }
 
-    // Конец
+    play(city) {
+        if (this.arr.length === 0) {
+            this.arr.push(city);
+            return this.arr;
+        } else {
+            if (city[0].toLowerCase() === this.arr[this.arr.length - 1][this.arr[this.arr.length - 1].length - 1]) {
+                this.arr.push(city);
+                return this.arr;
+            } else {
+                let winner = "";
+                if (this.arr.length % 2 === 0) {
+                    winner = this.name2;
+                } else {
+                    winner = this.name1;
+                }
+                return "Game over! The winner is " + winner;
+            }
+        }
+    }
+
+    restart() {
+        this.arr = [];
+        return "The game has been restarted!";
+    }
 }
 
 export default CityGame;
