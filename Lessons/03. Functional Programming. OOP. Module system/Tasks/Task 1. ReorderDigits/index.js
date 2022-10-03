5efbe0a9e34131d40522e8234637330bd15223b0
Semen Savelev <ctvbxm@mail.ru>
2022-10-03 17:06:50 +0300

    [UPD] Homework 3 finally fixed task1

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 1. ReorderDigits/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 1. ReorderDigits/index.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 const reorderDigits = (rule, ...numbers) => {
     if (rule === 'asc') return numbers.sort((a, b) => Number(a) - Number(b));
     else if (rule === 'desc') return numbers.sort((a, b) => Number(b) - Number(a));
-=======
-const reorderDigits = (type, ...numbers) => {
-    // Начало
-
-    // Конец
->>>>>>> origin
 };
 
 export default reorderDigits;
