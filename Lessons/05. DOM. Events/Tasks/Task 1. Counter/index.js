4a2d57f97bc2eaab0947ee61808fbf17d6413018
DariaGazkaeva <d.gazkaeva@gmail.com>
2022-10-09 19:57:16 +0100

    finished lesson4.tasks

--- Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
@@ -1,6 +1,16 @@
 const setCounter = () => {
     // Начало
-
+    const mainElement = document.querySelector('.main');
+    const button = document.createElement('button');
+    button.className = 'btn btn-warning';
+    button.textContent = 'Количество нажатий: 0';
+    mainElement.append(button);
+    button.addEventListener('click', (event) => {
+        const currentElement = event.target;
+        const text = currentElement.textContent;
+        const number = Number(text.split(': ')[1]);
+        currentElement.textContent = 'Количество нажатий: ' + (number + 1);
+    })
     // Конец
 };
 
