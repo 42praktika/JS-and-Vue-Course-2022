894698c76ad44fe84729cbd936c9c0244532b786
Semen Savelev <ctvbxm@mail.ru>
2022-10-10 21:48:31 +0300

    [ADD] Initialize Project 1

---
+++ Projects/Project 1. Native JS/js/build/core.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./js/index.js ***!
+  \*********************/
+alert('Вроде работает. УРА!');
+/******/ })()
+;
+//# sourceMappingURL=core.bundle.js.map
---
+++ Projects/Project 1. Native JS/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const EncodingPlugin = require('webpack-encoding-plugin');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+
+let developmentMode;
+module.exports = {
+
+    mode: process.env.NODE_ENV || 'development',
+
+
+    entry: {
+        'core': './js/index.js'
+    },
+
+
+    output: {
+        path: path.resolve(__dirname, './js/build/'),
+        filename: '[name].bundle.js',
+    },
+
+    module: {
+
+        rules: [
+
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: path.resolve(__dirname, './node_modules/')
+            },
+
+            {
+                test: /\.css$/,
+                use: [
+                    'css-loader',
+                    'style-loader',
+                ]
+            },
+        ]
+    },
+
+
+    plugins: [
+        new EncodingPlugin({
+            encoding: 'utf-8'
+        }),
+        new MomentLocalesPlugin({
+            localesToKeep: ['ru'],
+        }),
+    ],
+
+    resolve: {
+
+        alias: {
+            '@': path.resolve(__dirname, 'js/')
+        }
+    },
+
+
+    devtool: developmentMode ? 'eval-source-map' : 'cheap-source-map'
+};
