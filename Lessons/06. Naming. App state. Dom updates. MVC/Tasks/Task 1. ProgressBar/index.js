30e3613aed437eaecaa3e219dcbffe87f62983e7
KseniaKatargina <ksekat01@mail.ru>
2022-10-15 20:48:32 +0300

    fixed bugs in project and did tasks from lesson 6

--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
@@ -1,6 +1,31 @@
 const updateProgress = (defaultNumber, delta) => {
     // Начало
+    const state  = {
+        percent : defaultNumber,
+    };
+    let progressBar = document.querySelector(".progress-bar");
+    progressBar.style.width = state.percent + '%';
 
+    const increase = document.querySelector('.button-increase');
+
+    increase.addEventListener('click', () => {
+        if ((state.percent + delta) < 100) {
+            state.percent += delta;
+        } else {
+            state.percent = 100;
+        }
+        progressBar.style.width = state.percent + '%';
+    });
+
+    const decrease = document.querySelector('.button-decrease');
+    decrease.addEventListener('click',() => {
+        if ((state.percent - delta) > 0) {
+            state.percent -= delta;
+        } else {
+            state.percent = 0;
+        }
+        progressBar.style.width = state.percent + '%';
+    });
     // Конец
 };
 
--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 2. Carousel/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 2. Carousel/index.js
@@ -1,5 +1,50 @@
 const setCarousel = (imageUrls) => {
     // Начало
+    let carousel = document.querySelector('.carousel-inner');
+
+    const render = (state) => {
+        const items = document.querySelectorAll('.carousel-item');
+        let oldActive = document.querySelector('.active');
+        oldActive.classList.remove('active');
+        items[state.currImg].classList.add('active');
+    }
+
+    const state = {
+        currImg : 0,
+    };
+
+    imageUrls.forEach((image) => {
+        let div = document.createElement('div');
+        div.className = 'carousel-item';
+        let img = document.createElement('img');
+        img.classList.add('d-block', 'w-100');
+        img.src = image;
+        div.append(img);
+        carousel.append(div);
+    });
+
+    document.querySelector('.carousel-item').classList.add('active');
+
+    const nextImg = document.querySelector('.carousel-control-next');
+
+    nextImg.addEventListener('click', () => {
+        if (state.currImg < imageUrls.length - 1) {
+            state.currImg++;
+        } else {
+            state.currImg = 0;
+        }
+        render(state);
+    });
+
+    const prevImg = document.querySelector('.carousel-control-prev');
+    prevImg.addEventListener('click', () => {
+        if (state.currImg === 0) {
+            state.currImg = imageUrls.length - 1;
+        } else {
+            state.currImg--;
+        }
+        render(state);
+    });
 
     // Конец
 };
