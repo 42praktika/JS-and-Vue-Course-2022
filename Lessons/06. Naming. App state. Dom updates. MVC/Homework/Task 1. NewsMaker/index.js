7fb463cb4593953a0bc6f3a2d3c940f5dd8d934c
SNick22 <azat3310977@gmail.com>
2022-10-16 21:50:45 +0300

    copleted lesson 6

--- Lessons/06. Naming. App state. Dom updates. MVC/Homework/Task 1. NewsMaker/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Homework/Task 1. NewsMaker/index.js
@@ -2,9 +2,40 @@ import WatchJS from '../../helpers/melanke-watchjs.js';
 
 const { watch } = WatchJS;
 
+const watchState = (state) => {
+    const newsContainer = document.querySelector('.news__container');
+    watch(state, 'date', () => {
+        const newsElement = document.createElement('div');
+        newsElement.className = 'news__news-element';
+        newsContainer.prepend(newsElement)
+        const date = document.createElement('h5');
+        date.textContent = state.date;
+        newsElement.append(date);
+        const text = document.createElement('div');
+        text.textContent = state.text;
+        newsElement.append(text);
+        const formControl = document.querySelector('.form-control');
+        formControl.value = '';
+    });
+}
+
+const app = () => {
+    const state = {
+        date: 0,
+        text: 0,
+    }
+    const formControl = document.querySelector('.form-control');
+    const button = document.querySelector('.btn-primary');
+    button.addEventListener('click', () => {
+        state.date = new Date().toLocaleString();
+        state.text = formControl.value;
+    });
+    watchState(state);
+}
+
 const setNewsMaker = () => {
     // Начало
-
+    app();
     // Конец
 };
 
