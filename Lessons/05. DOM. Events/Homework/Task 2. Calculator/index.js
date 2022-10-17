6267a0a09d588860d5aa1e39c1ff7c03a356ec7d
Safiullin.Ayaz <tony.holl2609@gmail.com>
2022-10-17 14:59:37 +0300

    [ADD] L5 HW T2

--- Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
+++ Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
@@ -21,9 +21,52 @@ const ADDITIONAL_BUTTONS = [
 ];
 
 const setCalculator = () => {
-    // Начало
+    const calcMain = document.querySelector('.calc__main');
+    const calcButtons = document.querySelector('.calc__buttons');
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+        const digitBtn = document.createElement('button');
+        digitBtn.dataset.type = TYPES.DIGIT;
+        digitBtn.textContent = i.toString();
+        digitBtn.className = 'btn btn-dark';
+        digitBtn.addEventListener('click', event => {
+            calcMain.textContent += digitBtn.textContent;
+        })
+        calcButtons.append(digitBtn);
+    }
+    for (let i = 0; i < 3; i++) {
+        const btn = document.createElement('button');
+        btn.className = "btn btn-dark";
+        btn.textContent = ADDITIONAL_BUTTONS[i].text;
+        btn.dataset.type = ADDITIONAL_BUTTONS[i].type;
+        btn.addEventListener('click', event => {
+            if (i === 0) {
+                calcMain.textContent += btn.textContent;
+            }
+            if (i === 1) {
+                const resString = calcMain.textContent.split('+');
+                for (let i = 0; i < resString.length; i++) {
+                    result += Number(resString[i]);
+                }
+                spanRes.textContent = result.toString();
+            }
+            if (i === 2) {
+                calcMain.textContent = '';
+                spanRes.textContent = '0';
+                result = 0;
+            }
+        })
+        calcButtons.append(btn);
+    }
+    const calcResult = document.querySelector('.calc__result');
+    const spanResText = document.createElement('span');
+    spanResText.textContent = 'Результат:';
+    calcResult.append(spanResText);
+    const spanRes = document.createElement('span');
+    spanRes.textContent = result.toString();
+    spanRes.className = 'calc__result-number';
+    calcResult.append(spanRes);
 
-    // Конец
 };
 
 export default setCalculator;
