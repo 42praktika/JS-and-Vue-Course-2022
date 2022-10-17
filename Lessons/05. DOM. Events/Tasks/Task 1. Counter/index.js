1100bc5277dca6fe5d7e0d91cec09dc65f419266
askarik <askarsaliahov@gmail.com>
2022-10-17 21:14:51 +0300

    Classwork 5 , task 1

--- Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
@@ -1,7 +1,18 @@
 const setCounter = () => {
-    // Начало
+    const element = document.querySelector('.main');
+    const button = document.createElement('button');
+    button.className = 'btn btn-warning';
 
-    // Конец
+    element.appendChild(button);
+
+    let count = 0;
+    button.addEventListener('click', () => {
+        count++;
+        return button.textContent = 'Количество нажатий: ' + count;
+    })
+
+    button.textContent = 'Количество нажатий: ' + count;
+
 };
 
 export default setCounter;
