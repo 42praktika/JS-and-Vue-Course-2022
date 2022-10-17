a333149bf0c92251f5727d522bdde202dbe172c0
askarik <askarsaliahov@gmail.com>
2022-10-17 21:23:13 +0300

    Classwork 5, task 2

--- Lessons/05. DOM. Events/Tasks/Task 2. CreateButton/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 2. CreateButton/index.js
@@ -1,7 +1,17 @@
 const createButton = () => {
-    // Начало
+    const element = document.querySelector('.button-container');
+    const newButton = document.createElement('button')
+    newButton.className='btn btn-primary'
+    newButton.textContent="Кнопка"
+    element.append(newButton)
 
-    // Конец
+
+    newButton.addEventListener("click",()=>{
+        const element2=document.querySelector('.description')
+        const newSpan=document.createElement('span')
+        newSpan.textContent="Текст вставлен!"
+        element2.append(newSpan)
+    });
 };
 
 export default createButton;
