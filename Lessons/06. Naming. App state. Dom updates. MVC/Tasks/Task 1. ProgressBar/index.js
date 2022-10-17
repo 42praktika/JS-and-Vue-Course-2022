6cad4224aff4441a84795d5ad4567835820e337f
Dmitriy.Aleksandrov <Dmitriy.Aleksandrov@tatar.ru>
2022-10-17 21:32:23 +0300

    Revert "Есть какие-нибудь заметки?"
    This reverts commit 343777e950a36f5624f7037092458625d823247f.

--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
@@ -1,24 +1,7 @@
 const updateProgress = (defaultNumber, delta) => {
     // Начало
-    let number = defaultNumber;
-    const increaseButton = document.querySelector('.button-increase');
-    const decreaseButton = document.querySelector('.button-decrease');
 
-    const progressBar = document.querySelector('.progress-bar');
-    const setWidth = (number) => {
-        progressBar.setAttribute('style', `width: ${number}%`);
-    };
-    setWidth(number);
-
-    increaseButton.addEventListener('click', () => {
-        number = Math.min(number + delta, 100);
-        setWidth(number);
-    });
-    decreaseButton.addEventListener('click', () => {
-        number = Math.max(number - delta, 0);
-        setWidth(number);
-    });
     // Конец
 };
 
-module.exports = updateProgress;
+export default updateProgress;
