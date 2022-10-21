b37d4dddbb688b6cb76e58032072610eb2f50f4e
KseniaKatargina <ksekat01@mail.ru>
2022-10-22 00:01:28 +0300

    did homework from lesson 6

--- Lessons/06. Naming. App state. Dom updates. MVC/Homework/Task 1. NewsMaker/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Homework/Task 1. NewsMaker/index.js
@@ -4,6 +4,44 @@ const { watch } = WatchJS;
 
 const setNewsMaker = () => {
     // Начало
+    const app = () => {
+
+        const state = {
+            date: '',
+            text: '',
+        };
+
+        const button = document.querySelector('.btn-primary');
+        const input = document.querySelector('input');
+
+        button.addEventListener('click', () => {
+            init();
+            state.date = new Date().toLocaleString();
+            state.text = input.value;
+            input.value = '';
+        });
+        watchState(state);
+    };
+
+    const watchState = (state) => {
+        watch(state, () => {
+            document.querySelector('h5').textContent = state.date;
+            document.querySelector('.newsText').textContent = state.text;
+        });
+    };
+
+    const init = () => {
+        const container = document.querySelector('.news__container');
+        const newElem = document.createElement('div');
+        newElem.className = 'news__news-element';
+        const createDate = document.createElement('h5');
+        const newsText = document.createElement('div');
+        newsText.className = 'newsText';
+        newElem.append(createDate, newsText);
+        container.prepend(newElem);
+    };
+
+    app();
 
     // Конец
 };
