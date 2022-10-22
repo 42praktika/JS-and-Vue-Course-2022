c0950f10d817066541a84fcc7afaa2e3d27d1cc6
Igtisamova.Regina <reginaigtisamova@gmail.com>
2022-10-22 12:06:55 +0300

    Задачи 6 урокаÐ

--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
@@ -1,7 +1,35 @@
 const updateProgress = (defaultNumber, delta) => {
-    // Начало
+    const progress_bar = document.querySelector('.progress-bar')
+    const increase_button = document.querySelector('.button-increase')
+    const decrease_button = document.querySelector('.button-decrease')
 
-    // Конец
+    let status = defaultNumber
+    const render = () => {
+        progress_bar.style.width = `${status}%`
+    }
+    render(progress_bar)
+
+    const validate = (num) => {
+        if (num > 100) {
+            return 100
+        }
+        else if (num < 0) {
+            return 0
+        }
+        else {
+            return num
+        }
+    }
+
+    increase_button.addEventListener('click', () => {
+        status = validate(status + delta)
+        render()
+    })
+
+    decrease_button.addEventListener('click', () => {
+        status = validate(status - delta)
+        render()
+    })
 };
 
 export default updateProgress;
--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 2. Carousel/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 2. Carousel/index.js
@@ -1,7 +1,50 @@
 const setCarousel = (imageUrls) => {
-    // Начало
+    const carousel = document.querySelector(".carousel-inner")
 
-    // Конец
+    const render = (status) => {
+        const items = document.querySelectorAll('.carousel-item')
+        let carousel_active = document.querySelector('.active')
+        carousel_active.classList.remove('active')
+        items[status.current_img].classList.add('active')
+    }
+    const state = {
+        current_img : 0,
+    }
+
+    imageUrls.forEach((image) => {
+        let div = document.createElement('div')
+        div.className = 'carousel-item'
+
+        let img = document.createElement('img')
+        img.classList.add('d-block', 'w-100')
+        img.src = image
+
+        div.append(img)
+        carousel.append(div)
+    })
+    document.querySelector('.carousel-item').classList.add('active');
+    const next_img = document.querySelector('.carousel-control-next');
+
+    next_img.addEventListener('click', () => {
+        if (state.current_img < imageUrls.length - 1) {
+            state.current_img += 1
+        }
+        else {
+            state.current_img = 0
+        }
+        render(state)
+    })
+
+    const prev_img = document.querySelector('.carousel-control-prev');
+    prev_img.addEventListener('click', () => {
+        if (state.current_img === 0) {
+            state.current_img = imageUrls.length - 1
+        }
+        else {
+            state.current_img -= 1
+        }
+        render(state)
+    })
 };
 
 export default setCarousel;
