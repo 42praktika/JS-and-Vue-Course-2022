75edc77abe94f0b6dc226c6ece112a7069193af0
Hehpheh <102379707+Hehpheh@users.noreply.github.com>
2022-10-23 14:18:36 +0300

    adeded tasks

--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
@@ -1,5 +1,33 @@
 const updateProgress = (defaultNumber, delta) => {
     // Начало
+    const buttonIincrease = document.querySelector('.button-increase');
+    const buttonDecrease = document.querySelector('.button-decrease');
+    const progressBar = document.querySelector('.progress-bar');
+    let progress = defaultNumber;
+
+    const watch = () => {
+        progressBar.style.width = progress + '%';
+    };
+
+
+    buttonIincrease.addEventListener('click', () => {
+        if ((progress + delta) < 100) {
+            progress += delta;
+        } else {
+            progress = 100;
+        }
+        watch();
+    });
+
+    buttonDecrease.addEventListener('click', () => {
+        if ((progress - delta) > 0) {
+            progress -= delta;
+        } else {
+            progress = 0;
+        }
+        watch();
+    });
+   watch();
 
     // Конец
 };
