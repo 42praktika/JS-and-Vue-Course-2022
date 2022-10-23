1696288037ef454bf9931971ab8f1277af9fd801
KipeTBest <90268471+KipeTBest@users.noreply.github.com>
2022-10-23 13:47:34 +0300

    HomeWork
    Added classTasks from 6 day

--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
@@ -1,6 +1,42 @@
 const updateProgress = (defaultNumber, delta) => {
     // Начало
+    const state = {
+        defaultWidth: defaultNumber,
+        value: delta
+    }
 
+    const increaseButton = document.querySelector('.button-increase');
+    const decreaseButton = document.querySelector('.button-decrease');
+
+    const progressBar = document.querySelector('.progress-bar');
+    progressBar.setAttribute('aria-valuemin', '0');
+    progressBar.setAttribute('aria-valuemax', '100');
+    progressBar.setAttribute('role', 'progressbar');
+    progressBar.setAttribute('style', `width: ${state.defaultWidth}%`);
+
+    const setWidth = (num) => {
+        progressBar.setAttribute('style', `width: ${num}%`);
+    }
+
+    increaseButton.addEventListener('click', () => {
+        let newWidth = state.defaultWidth + state.value;
+        if (state.defaultWidth <= 100 && newWidth <= 100) {
+            state.defaultWidth = newWidth;
+        } else if (state.defaultWidth <= 100 && newWidth > 100) {
+            state.defaultWidth = 100;
+        }
+        setWidth(state.defaultWidth);
+    })
+
+    decreaseButton.addEventListener('click', () => {
+        let newWidth = state.defaultWidth - state.value;
+        if (state.defaultWidth >= 0 && newWidth >= 0) {
+            state.defaultWidth -= state.value;
+        } else if (state.defaultWidth >= 0 && newWidth < 0) {
+            state.defaultWidth = 0;
+        }
+        setWidth(state.defaultWidth);
+    })
     // Конец
 };
 
--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 2. Carousel/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 2. Carousel/index.js
@@ -1,6 +1,65 @@
 const setCarousel = (imageUrls) => {
     // Начало
+    const state = {
+        currentIndexOfUrl: 0
+    }
 
+    // Своего рода костыль
+    let arrayOfUrls = [];
+    for (let i = 0; i < imageUrls.length; i++) {
+        arrayOfUrls.push(imageUrls[i]);
+    }
+
+    const carouselInner = document.querySelector('.carousel-inner');
+    const nextButton = document.querySelector('.carousel-control-next');
+    const previousButton = document.querySelector('.carousel-control-prev');
+
+    // Еще костыль
+    const firstIndex = 0;
+    let iterator = 0;
+
+    arrayOfUrls.forEach((url) => {
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+
+        div.append(img);
+        carouselInner.append(div);
+
+        // Продолжение костыля
+        if (iterator === firstIndex) {
+            div.className = 'carousel-item active';
+        } else div.className = 'carousel-item';
+
+        img.classList.add('d-block', 'w-100');
+        img.src = url;
+
+        iterator++;
+    })
+
+    const changeActivity = (urlIndex) => {
+        const allUrls = document.querySelectorAll('.carousel-item');
+        for (let i = 0; i < allUrls.length; i++) {
+            if (i === urlIndex) {
+                allUrls[i].classList.add('active');
+            } else allUrls[i].classList.remove('active');
+        }
+    };
+
+    nextButton.addEventListener('click', () => {
+        if (state.currentIndexOfUrl < arrayOfUrls.length - 1) {
+            state.currentIndexOfUrl++;
+        } else state.currentIndexOfUrl = 0;
+
+        changeActivity(state.currentIndexOfUrl);
+    })
+
+    previousButton.addEventListener('click', () => {
+        if (state.currentIndexOfUrl > 0) {
+            state.currentIndexOfUrl--;
+        } else state.currentIndexOfUrl = arrayOfUrls.length - 1;
+
+        changeActivity(state.currentIndexOfUrl);
+    })
     // Конец
 };
 
