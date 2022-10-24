bee6d58377f305e80f4f5cc869c9fe5ac5bf5fb0
askarik <askarsaliahov@gmail.com>
2022-10-24 18:01:53 +0300

    Homework 6 ,task1

--- Lessons/06. Naming. App state. Dom updates. MVC/Homework/Task 1. NewsMaker/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Homework/Task 1. NewsMaker/index.js
@@ -1,11 +1,29 @@
-import WatchJS from '../../helpers/melanke-watchjs.js';
+import app from './app.js';
 
-const { watch } = WatchJS;
+/*const setNewsMaker = () => {
+    const classNewsContainer = document.querySelector('.news__container')
+    const buttonAddNews = document.querySelector('.btn-primary')
+    const input = document.querySelector('input');
 
+    buttonAddNews.addEventListener('click', () => {
+        const Div = document.createElement('div')
+        Div.className = 'news__news-element'
+        classNewsContainer.append(Div)
+
+        const H5 = document.createElement('h5');
+        const divForNews = document.createElement('div')
+        const d = new Date();
+        let text = d.toLocaleString();
+
+        Div.append(H5)
+        H5.append(text)
+        Div.append(divForNews)
+        divForNews.append(input.value)
+        input.value = ''
+    });
+}*/
 const setNewsMaker = () => {
-    // Начало
-
-    // Конец
-};
+    app();
+}
 
 export default setNewsMaker;
