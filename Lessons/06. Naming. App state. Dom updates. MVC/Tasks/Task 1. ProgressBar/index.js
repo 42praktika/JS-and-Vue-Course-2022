9601f2c18f9f107bc268046908268f665968276b
askarik <askarsaliahov@gmail.com>
2022-10-24 18:12:56 +0300

    Classwork 6, task 1

--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
@@ -1,7 +1,35 @@
-const updateProgress = (defaultNumber, delta) => {
-    // Начало
+const render = (state) => {
+    const classProgressBar = document.querySelector('.progress-bar');
+    classProgressBar.style.width = state.percent + '%';
+}
 
-    // Конец
-};
+const updateProgress = (defaultNumber, delta) => {
+    const state = {
+        percent: defaultNumber,
+    };
+
+    const buttonNext = document.querySelector('.button-increase');
+    buttonNext.addEventListener('click', () => {
+        if (state.percent + delta < 100) {
+            state.percent += delta;
+        } else {
+            state.percent = 100;
+        }
+        render(state);
+    });
+
+    const buttonBehind = document.querySelector('.button-decrease');
+    buttonBehind.addEventListener('click', () => {
+        if (state.percent - delta > 0) {
+            state.percent -= delta;
+        } else {
+            state.percent = 0;
+        }
+        render(state);
+    })
+
+
+    render(state);
+}
 
 export default updateProgress;
