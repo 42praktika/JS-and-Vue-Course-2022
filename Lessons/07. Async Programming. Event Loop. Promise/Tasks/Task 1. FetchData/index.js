82079e8441458a81ccfeb6a987b2d5b6db2c3fc3
askarik <askarsaliahov@gmail.com>
2022-10-25 13:20:08 +0300

    Classwork 7,task 1

--- Lessons/07. Async Programming. Event Loop. Promise/Tasks/Task 1. FetchData/index.js
+++ Lessons/07. Async Programming. Event Loop. Promise/Tasks/Task 1. FetchData/index.js
@@ -1,7 +1,12 @@
 const fetchData = () => {
-    // Начало
-
-    // Конец
+    let timeoutID;
+    let input=document.querySelector('.form-control')
+    input.addEventListener('input',()=>{
+        clearTimeout(timeoutID);
+        timeoutID=setTimeout(function () {
+            alert("Ищем: "+input.value)
+        },1000)
+    })
 };
 
 export default fetchData;
