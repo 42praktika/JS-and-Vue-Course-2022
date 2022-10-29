13c0653c4cd261f55a3a38aabf405719e0c8c493
DariaGazkaeva <d.gazkaeva@gmail.com>
2022-10-29 20:35:14 +0100

    Implement the basic work of modal windows

--- Projects/Project 1. Native JS/js/index.js
+++ Projects/Project 1. Native JS/js/index.js
@@ -1,2 +1,45 @@
-alert('New Message')
-alert('NEW NEW')
+const bodyElement = document.querySelector('body');
+const buttons = document.querySelectorAll('.button');
+const closeButtons = document.querySelectorAll('.close-modal');
+const prevButtons = document.querySelectorAll('.prev-modal');
+const nextButtons = document.querySelectorAll('.next-modal');
+
+const TYPES = {
+    'ANIME': document.querySelector('div.modal-anime'),
+    'ART-AND-DESIGN': document.querySelector('div.modal-art-and-design'),
+    'CALENDAR': document.querySelector('div.modal-calendar'),
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        openWindow(button);
+    })
+})
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentElement.style.display = 'none';
+        bodyElement.style.backgroundImage = 'url("img/body_background_first.jpg")';
+    })
+})
+
+prevButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentElement.style.display = 'none';
+        TYPES[button.getAttribute('data-type')].style.display = 'block';
+    })
+})
+
+nextButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentElement.style.display = 'none';
+        TYPES[button.getAttribute('data-type')].style.display = 'block';
+    })
+})
+
+const openWindow = (button) => {
+    const type = button.getAttribute('data-type');
+    TYPES[type].style.display = 'block';
+    bodyElement.style.backgroundImage = 'url("img/body_background_second.jpg")';
+}
+
