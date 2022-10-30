1c4bc221ed26c872f07e274ea36fcef2f9e2a74c
Semen Savelev <ctvbxm@mail.ru>
2022-10-30 22:01:36 +0300

    [UPD] Homework 7 refactored

--- Lessons/07. Async Programming. Event Loop. Promise/Tasks/Task 3. WriteSum Async/index.js
+++ Lessons/07. Async Programming. Event Loop. Promise/Tasks/Task 3. WriteSum Async/index.js
@@ -1,6 +1,5 @@
-import { promises as fs } from 'fs';
+import {promises as fs} from 'fs';
 import path from 'path';
-import {throwError} from "@vue/vue2-jest/lib/utils";
 
 // Метод для отладки. В итоговом решении использоваться не должен
 const getPath = (fileName) => path.join(__dirname, './__fixtures__', fileName);
@@ -9,22 +8,21 @@ const getPath = (fileName) => path.join(__dirname, './__fixtures__', fileName);
 
 const writeSum = (pathToFileOne, pathToFileTwo) => {
     let totalSum = 0;
-
-     return new Promise(function(resolve, reject) {
-        fs.readFile(pathToFileOne, 'utf-8')
-            .then((data) => {
-                data.split(', ').forEach(num => {totalSum += Number(num)});
-            })
-            .catch(() => {reject(new Error('Такого файла нет'))})
-            .then(() => {
-                fs.readFile(pathToFileTwo, 'utf-8')
-                    .then((data) => {
-                        data.split(', ').forEach(num => {totalSum += Number(num)});
-                    })
-                    .catch(() => {reject(new Error('Такого файла нет'))})
-                    .then(() => {resolve(totalSum)});
+    return fs.readFile(pathToFileOne, 'utf-8')
+        .then((data) => {
+            data.split(', ').forEach(num => {
+                totalSum += Number(num)
             });
-    });
-};
+        })
+        .catch((error) => console.error('Такого файла нет'))
+        .then(() => fs.readFile(pathToFileTwo, 'utf-8'))
+        .then((data) => {
+            data.split(', ').forEach(num => {
+                totalSum += Number(num)
+            });
+        })
+        .catch((error) => console.error('Такого файла нет'))
+        .then(() => totalSum);
+}
 
 export default writeSum;
