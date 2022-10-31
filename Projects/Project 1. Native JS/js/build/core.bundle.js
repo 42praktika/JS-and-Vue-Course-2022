ef13b386c1cfa03b20eb1e7c108c1733ff8e2095
Safiullin.Ayaz <tony.holl2609@gmail.com>
2022-10-31 16:06:06 +0300

    [ADD] Not done Project!!!

--- Projects/Project 1. Native JS/js/build/core.bundle.js
+++ Projects/Project 1. Native JS/js/build/core.bundle.js
@@ -3,7 +3,25 @@ var __webpack_exports__ = {};
 /*!*********************!*\
   !*** ./js/index.js ***!
   \*********************/
-alert('Привет!');
+var prostoObject = {
+  info1: {
+    //НАЗВАНИЯ ПОТОМ СМЕНЮ. И университет
+    dataName: "info1"
+  },
+  info2: {
+    dataName: "info2"
+  },
+  info3: {
+    dataName: "info3"
+  }
+};
+var btnContainer = document.querySelector('.buttons');
+btnContainer.addEventListener('click', function (event) {
+  var buttons = document.querySelector('.buttons');
+  buttons.style.display = 'none';
+  var modal = document.querySelector('.modal-' + event.target.dataset.type);
+  modal.style.display = 'block';
+});
 /******/ })()
 ;
 //# sourceMappingURL=core.bundle.js.map
--- Projects/Project 1. Native JS/js/index.js
+++ Projects/Project 1. Native JS/js/index.js
@@ -1,1 +1,19 @@
-alert('Привет!');
+const prostoObject = {
+    info1: {                  //НАЗВАНИЯ ПОТОМ СМЕНЮ. И университет
+        dataName: "info1"
+    },
+    info2: {
+        dataName: "info2"
+    },
+    info3: {
+        dataName: "info3"
+    }
+}
+
+const btnContainer = document.querySelector('.buttons');
+btnContainer.addEventListener('click', event => {
+    const buttons = document.querySelector('.buttons')
+    buttons.style.display = 'none';
+    const modal = document.querySelector('.modal-' + event.target.dataset.type);
+    modal.style.display = 'block';
+})
