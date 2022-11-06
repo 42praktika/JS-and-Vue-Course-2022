06f5cddc34d6dc6682febf0bf3af9a057a19f1f9
AskarSaliakhov <90134116+AskarSaliakhov@users.noreply.github.com>
2022-11-06 16:44:05 +0300

    Classwork 8, task 2

--- Lessons/08. Async Programming. Async-Await. AJAX/Tasks/Task 2. GiveMeMeme/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Tasks/Task 2. GiveMeMeme/index.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://api.imgflip.com/get_memes';
 
 const updateImage = (imageData) => {
-    const { url, width, height } = imageData;
+    const {url, width, height} = imageData;
 
     const imageContainer = document.querySelector('.main__image-container');
     const imgElement = document.createElement('img');
@@ -16,8 +16,26 @@ const updateImage = (imageData) => {
 const getRandomIndex = () => Math.floor(100 * Math.random());
 
 const giveMeMeme = () => {
-    // Начало
-    // Конец
+    const button = document.createElement('button')
+    button.classList.add("btn", "btn-warning")
+    button.textContent = "ДАЙ МНЕ МЕМ!"
+
+    const divContainer = document.createElement('div')
+    divContainer.className = "main__image-container"
+
+    const mainClass = document.querySelector(".main")
+    mainClass.append(divContainer, button)
+
+    button.addEventListener('click', async () => {
+        try {
+            const response = await fetch(BASE_URL)
+            const responseData = await response.json()
+            updateImage(responseData.data.memes[getRandomIndex()])
+        } catch (error) {
+            throw error
+        }
+    })
+
 };
 
 export default giveMeMeme;
