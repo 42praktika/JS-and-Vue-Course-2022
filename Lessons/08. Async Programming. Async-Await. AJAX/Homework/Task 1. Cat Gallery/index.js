a848fb65695114a3d136aa3bbf65940b9451ffd0
AskarSaliakhov <90134116+AskarSaliakhov@users.noreply.github.com>
2022-11-07 20:30:16 +0300

    Homework8

--- Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
@@ -1,15 +1,18 @@
-// Импорт axios ниже работает только в node.js и нужен для тестирования
-// Для отслеживания решения в браузере axios добавляется на страницу в виде скрипта:
-// <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-// Выполните задание, отслеживая изменения в браузере,
-// после чего раскомментируйте строку с импортом для прохождения тестов
-
-// import axios from 'axios';
+import axios from "axios";
 
 const setCatGallery = () => {
-    // Начало
-
-    // Конец
+    const URL = "https://api.thecatapi.com/v1/images/search"
+    const COUNT_OF_PICTURES = 10
+    const mainContainer = document.querySelector('.main__container');
+    const addCats = async () => {
+        for (let i = 0; i < COUNT_OF_PICTURES; i++) {
+            const img = document.createElement('img');
+            let url = await axios.get(URL);
+            img.setAttribute('src', url.data[0].url);
+            mainContainer.append(img);
+        }
+    }
+    return addCats().then(() => "cat gallery is ready!");
 };
 
 export default setCatGallery;
