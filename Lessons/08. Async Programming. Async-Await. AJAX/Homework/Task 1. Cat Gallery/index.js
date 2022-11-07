b9e80f2d5c2bf45b2daf666f51dac8f8bd56c341
Hehpheh <102379707+Hehpheh@users.noreply.github.com>
2022-11-07 20:03:27 +0300

    added tasks
    added tasks

--- Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
@@ -4,11 +4,21 @@
 // Выполните задание, отслеживая изменения в браузере,
 // после чего раскомментируйте строку с импортом для прохождения тестов
 
-// import axios from 'axios';
+import axios from 'axios';
 
 const setCatGallery = () => {
     // Начало
-    
+    const addImg = async () => {
+        for (let i = 0; i <10; i++) {
+            let container = document.querySelector('.main__container');
+            let img = document.createElement('img');
+            let url = await axios.get('https://api.thecatapi.com/v1/images/search');
+            img.src = await url.data[0].url;
+            container.appendChild(img);
+        }
+    }
+    return addImg().then(() => "cat gallery is ready!");
+
     // Конец
 };
 
