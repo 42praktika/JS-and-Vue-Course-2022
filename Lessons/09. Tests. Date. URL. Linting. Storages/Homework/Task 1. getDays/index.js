ac691ee95324e2880a8670b6a333f5ee062dd8a2
KseniaKatargina <90481483+KseniaKatargina@users.noreply.github.com>
2022-11-12 14:37:56 +0300

    did tasks and homework from lesson 9

--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
@@ -1,6 +1,25 @@
+const monthWith31Days = ['January', 'March', 'May', 'July', 'August', 'October', 'December'];
+const monthWith30Days = ['April','June', 'September', 'November'];
+
+const isLeapYear = (year) => {
+    if (new Date(year,1,29).getDate() === 29) {
+        return 29;
+    } else {
+        return 28;
+    }
+}
+
 const getDaysAmount = (month, year) => {
     // Начало
-
+    if (monthWith30Days.includes(month)) {
+        return 30;
+    } else if (monthWith31Days.includes(month)) {
+        return 31;
+    } else if (month === 'February') {
+        return isLeapYear(year);
+    } else {
+        return 'error';
+    }
     // Конец
 };
 
--- Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 2. Tests/index.test.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 2. Tests/index.test.js
@@ -2,7 +2,69 @@ import getIsFirstArrayCooler from './index';
 
 describe('09.c.2 getIsFirstArrayCooler', () => {
     // Начало
-    it('', () => {});
+    let arr1;
+    let arr2;
 
+    beforeEach(() => {
+        arr1= [];
+        arr2= [];
+    })
+
+
+    it('09.c.2.1 getIsFirstArrayCooler is defined', () => {
+        arr1.push(1, 2, 3);
+        arr2.push(1, 2, 4);
+        expect(getIsFirstArrayCooler(arr1, arr2)).toBeDefined();
+    });
+
+    it('09.c.2.2 work correct with empty arrays', () => {
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(false);
+    });
+
+    it('09.c.2.3 work correct when first array is empty', () => {
+        arr2.push(1);
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(false);
+    });
+
+    it('09.c.2.4 work correct when second array is empty', () => {
+        arr1.push(1);
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(false);
+    });
+
+    it('09.c.2.5 work correct with arrays of different lengths', () => {
+        arr1.push(1);
+        arr2.push(2, 3, 4);
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(false);
+    });
+
+    it('09.c.2.6 work correct when second array is cooler than first', () => {
+        arr1.push(1, 4, 3);
+        arr2.push(2, 3, 4);
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(false);
+    });
+
+    it('09.c.2.7 work correct with equal arrays', () => {
+        arr1.push(1, 2, 3);
+        arr2.push(1, 2, 3);
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(false);
+    });
+
+    it('09.c.2.8 work correct when first array is cooler than second', () => {
+        arr1.push(3, 4, 8);
+        arr2.push(1, 6, 3);
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(true);
+    });
+
+    it('09.c.2.9 work correct with float numbers when first array is cooler than second', () => {
+        arr1.push(3.1 ,4.8 ,8.1);
+        arr2.push(1.2, 6.9, 3.2);
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(true);
+    });
+
+    it('09.c.2.10 work correct with float numbers when second array is cooler than first', () => {
+        arr1.push(3.1 ,4.8 ,8.1);
+        arr2.push(4.2, 6.9, 3.2);
+        expect(getIsFirstArrayCooler(arr1,arr2)).toBe(false);
+    });
     // Конец
 });
