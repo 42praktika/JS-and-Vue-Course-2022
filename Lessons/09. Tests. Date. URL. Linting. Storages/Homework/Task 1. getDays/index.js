16098e0171a45ee3641ad7114ad38ceafc21dadc
Zinnur41 <zagidullin_zin@mail.ru>
2022-11-13 20:36:41 +0300

    вВыполенÐнил дз 07

--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
@@ -1,5 +1,33 @@
+const daysInMonthsWithNotLeapYear = {
+    'January': 31,
+    'February' : 28,
+    'March': 31,
+    'April': 30,
+    'May': 31,
+    'June': 30,
+    'July': 31,
+    'August': 31,
+    'September': 30,
+    'October': 31,
+    'November': 30,
+    'December': 31,
+}
+
+const daysWithLeapYear = {
+    'FebruaryLeapYear': 29,
+}
 const getDaysAmount = (month, year) => {
     // Начало
+    if (daysInMonthsWithNotLeapYear[month] === 30 || daysInMonthsWithNotLeapYear[month] === 31) {
+        return daysInMonthsWithNotLeapYear[month];
+    } else if (month === 'February') {
+        if (year % 4 === 0) {
+            return daysWithLeapYear.FebruaryLeapYear;
+        }
+        return daysInMonthsWithNotLeapYear[month];
+    } else {
+        return 'error';
+    }
 
     // Конец
 };
--- Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 2. Tests/index.test.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 2. Tests/index.test.js
@@ -2,7 +2,71 @@ import getIsFirstArrayCooler from './index';
 
 describe('09.c.2 getIsFirstArrayCooler', () => {
     // Начало
-    it('', () => {});
+    let arrayNumsOne
+    let arrayNumsTwo
+    beforeEach(() => {
+         arrayNumsOne = [];
+         arrayNumsTwo = [];
+    })
+
+    it('c.8.2.1 getIsFirstArrayCooler work correct with arrays where length 0', () => {
+        expect(getIsFirstArrayCooler(arrayNumsOne, arrayNumsTwo)).toBe(false);
+    });
+
+    it('c.8.2.2 getIsFirstArrayCooler work correct with arrays where length is equal', () => {
+        arrayNumsOne = [1, 2, 3, 4, 5];
+        arrayNumsTwo = [1, 2, 3, 4, 5];
+        expect(getIsFirstArrayCooler(arrayNumsOne, arrayNumsTwo)).toBe(false);
+    })
+
+    it('c.8.2.3 getIsFirstArrayCooler work correct with arrays where one array is empty and second array is not empty', () => {
+        arrayNumsOne = [];
+        arrayNumsTwo = [1, 2, 3, 4, 5];
+        expect(getIsFirstArrayCooler(arrayNumsOne, arrayNumsTwo)).toBe(false);
+    })
+
+    it('c.8.2.4 getIsFirstArrayCooler work correct with arrays where one array is not empty and second array is empty', () => {
+        arrayNumsOne = [1, 2, 3, 4, 5];
+        arrayNumsTwo = [];
+        expect(getIsFirstArrayCooler(arrayNumsOne, arrayNumsTwo)).toBe(false);
+    })
+
+    it('c.8.2.5 getIsFirstArrayCooler work correct when arrays of different lengths', () => {
+        arrayNumsOne = [1, 2, 3, 4, 5];
+        arrayNumsTwo = [1, 2, 3];
+        expect(getIsFirstArrayCooler(arrayNumsOne, arrayNumsTwo)).toBe(false);
+    })
+
+    it('c.8.2.6 getIsFirstArrayCooler work correct when arrays of different lengths', () => {
+        arrayNumsOne = [1, 2, 3];
+        arrayNumsTwo = [1, 2, 3, 4, 5];
+        expect(getIsFirstArrayCooler(arrayNumsOne, arrayNumsTwo)).toBe(false);
+    })
+
+    it('c.8.2.7 getIsFirstArrayCooler work correct when second array cooler than first', () => {
+        arrayNumsOne = [1, 2, 3, 4, 5];
+        arrayNumsTwo = [1, 2, 3, 5, 6];
+        expect(getIsFirstArrayCooler(arrayNumsOne, arrayNumsTwo)).toBe(false);
+    })
+
+    it('c.8.2.8 getIsFirstArrayCooler work correct when first array cooler than second ', () => {
+        arrayNumsOne = [1, 2, 3, 5, 6];
+        arrayNumsTwo = [1, 2, 3, 4, 5];
+        expect(getIsFirstArrayCooler(arrayNumsOne, arrayNumsTwo)).toBe(true);
+    })
+
+    it('c.8.2.9 getIsFirstArrayCooler work correct with float numbers when first array is cooler than second', () => {
+        arrayNumsOne = [2.1 ,3.1 ,4.1];
+        arrayNumsTwo = [1.1, 2.1, 3.1];
+        expect(getIsFirstArrayCooler(arrayNumsOne,arrayNumsTwo)).toBe(true);
+    });
+
+    it('c.8.2.10 getIsFirstArrayCooler work correct with float numbers when second array is cooler than first', () => {
+        arrayNumsOne = [2.1 ,3.1 ,4.1];
+        arrayNumsTwo = [3.1, 2.1, 5.1];
+        expect(getIsFirstArrayCooler(arrayNumsOne,arrayNumsTwo)).toBe(false);
+    });
+
 
     // Конец
 });
