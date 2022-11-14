93415d917dd1a103ed5508c9cf95dcae574a06df
askarik <askarsaliahov@gmail.com>
2022-11-14 16:32:11 +0300

    Homework 9,task 1

--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
@@ -1,7 +1,11 @@
+const months31Days = ['January', 'March', 'May', 'July', 'August', 'October', 'December'];
+const months30Days = ['April', 'June', 'September', 'November'];
 const getDaysAmount = (month, year) => {
-    // Начало
-
-    // Конец
+    if (months30Days.includes(month)) {return 30}
+    if (months31Days.includes(month)) {return 31}
+    if (year%4===0 && month==="February") {return 29}
+    if (year%4!==0 && month==="February") {return 28}
+    return 'error'
 };
 
 export default getDaysAmount;
