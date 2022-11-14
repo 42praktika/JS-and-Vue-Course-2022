367b94c79fd641b08ff1599d4a14fd94d9601184
Igtisamova.Regina <reginaigtisamova@gmail.com>
2022-11-14 12:49:57 +0300

    Задачи 9 урокаÐ

--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
@@ -1,7 +1,42 @@
-const getDaysAmount = (month, year) => {
-    // Начало
+const daysInEachMonth = {
+    'January': 31,
+    'February' : 28,
+    'March': 31,
+    'April': 30,
+    'May': 31,
+    'June': 30,
+    'July': 31,
+    'August': 31,
+    'September': 30,
+    'October': 31,
+    'November': 30,
+    'December': 31,
+}
 
-    // Конец
+const months = Object.keys(daysInEachMonth);
+
+const daysInFebruary = {
+    'leapYear': 29,
+    'notALeapYear': 28,
+}
+
+
+
+const getDaysAmount = (month, year) => {
+    if (daysInEachMonth[month] === 30 || daysInEachMonth[month] === 31) {
+        return daysInEachMonth[month]
+    }
+    else if (month === 'February') {
+        if (year % 4 === 0) {
+            return daysInFebruary.leapYear
+        }
+        else {
+            return daysInFebruary.notALeapYear
+        }
+    }
+    else {
+        return 'error'
+    }
 };
 
 export default getDaysAmount;
--- Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 2. Tests/index.test.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 2. Tests/index.test.js
@@ -1,8 +1,41 @@
 import getIsFirstArrayCooler from './index';
 
 describe('09.c.2 getIsFirstArrayCooler', () => {
-    // Начало
-    it('', () => {});
 
-    // Конец
+    const arrayOne = [1, 2, 3]
+    const arrayTwo = [1, 2]
+    const arrayThree = []
+    const arrayFour = [2, 3, 4]
+    const arrayFive = [4, 5, 6]
+    const arraySix = [2, 5, 4]
+
+    it('09.c.2.1 false if arrays of different lengths', () => {
+        expect(getIsFirstArrayCooler(arrayOne, arrayTwo)).toBe(false);
+        expect(getIsFirstArrayCooler(arrayTwo, arrayOne)).toBe(false);
+        expect(getIsFirstArrayCooler(arrayThree, arrayOne)).toBe(false);
+    });
+
+    it('09.c.2.1 false if the array is empty', () => {
+        expect(getIsFirstArrayCooler(arrayThree, arrayThree)).toBe(false);
+    });
+
+    it('09.c.2.3 false if all elements of the first array are less', () => {
+        expect(getIsFirstArrayCooler(arrayOne, arrayFour)).toBe(false);
+        expect(getIsFirstArrayCooler(arrayOne, arrayFive)).toBe(false);
+        expect(getIsFirstArrayCooler(arrayOne, arraySix)).toBe(false);
+    });
+
+    it('09.c.2.3 true if all elements of the first array are greater', () => {
+        expect(getIsFirstArrayCooler(arrayFour, arrayOne)).toBe(true);
+        expect(getIsFirstArrayCooler(arrayFive, arrayOne)).toBe(true);
+        expect(getIsFirstArrayCooler(arraySix, arrayOne)).toBe(true);
+    });
+
+    it('09.c.2.3 true if more elements in first array greater', () => {
+        expect(getIsFirstArrayCooler(arrayFive, arraySix)).toBe(true);
+    });
+
+    it('09.c.2.3 false if more elements in first array less', () => {
+        expect(getIsFirstArrayCooler(arraySix, arrayFive)).toBe(false);
+    });
 });
