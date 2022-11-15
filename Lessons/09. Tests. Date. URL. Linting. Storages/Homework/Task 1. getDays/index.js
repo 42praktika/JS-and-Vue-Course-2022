01b9071efe237192205d619615d993afdf03a5ae
Churaev.Adel <south.adel@mail.ru>
2022-11-15 10:12:22 +0300

    9th tasks added

--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
@@ -1,7 +1,23 @@
+const monthWith30Days = ['April','June', 'September', 'November'];
+const monthWith31Days = ['January', 'March', 'May', 'July', 'August', 'October', 'December'];
 const getDaysAmount = (month, year) => {
-    // Начало
 
-    // Конец
+    if(monthWith31Days.includes(month)) {
+        return 31;
+    }
+    if(monthWith30Days.includes(month)) {
+        return 30;
+    }
+    if(month === 'February' && year%4===0) {
+        return 29;
+    }
+    if(month === 'February' && year%4!==0) {
+        return 28;
+    }
+    else {
+        return 'error';
+    }
+
 };
 
 export default getDaysAmount;
--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 2. refactorMe/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 2. refactorMe/index.js
@@ -1,19 +1,22 @@
+const specialSymbols = {
+    'text=': ' ',
+    'coma': ',',
+    'dot': '.',
+    'space': ' ',
+    'semicolon': ';',
+    'apostrophe': "'",
+    '&': '',
+    '=': '',
+    "' ": "'"
+}
+
+
 const getTextFromUrl = (url) => {
-    // Начало
-    const o = new URL(url);
-    var par = o.searchParams;
-    var str = par.toString();
-    let rep_text = str.replaceAll('text=', ' ');
-    let rep_c = rep_text.replaceAll('coma', ',');
-    let rep_d = rep_c.replaceAll('dot', '.');
-    let space = rep_d.replaceAll('space', ' ');
-    let sc = space.replaceAll('semicolon', ';');
-    var apth = sc.replaceAll('apostrophe', "'");
-    var ampRepl = apth.replaceAll('&', '');
-    var ravno = ampRepl.replaceAll('=', '');
-    var deleteProbel = ravno.replaceAll("' ", "'");
-    return deleteProbel;
-    // Конец
+    let urlText = new URL(url).searchParams.toString();
+    for(let symbol in specialSymbols) {
+        urlText = urlText.replaceAll(symbol, specialSymbols[symbol]);
+    }
+    return urlText;
 };
 
 export default getTextFromUrl;
