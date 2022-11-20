9fe87d47622cd8db19c91b01683b100405203318
oyatullo277 <oyatillozokirjonov351@gmail.com>
2022-11-19 23:45:28 -1100

    Project

--- Projects/webpack.config.js
+++ Projects/webpack.config.js
@@ -1,7 +1,28 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: "[name].[contenthash].js",
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+    ],
 }
