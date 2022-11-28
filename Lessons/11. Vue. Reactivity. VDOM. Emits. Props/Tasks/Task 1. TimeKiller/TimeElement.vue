c628349821159e407768cb4149e80e91fe6869dc
LenaGavrilova <lena.gavrilova2408@mail.ru>
2022-11-28 21:59:57 +0300

    done 11 task

---
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeElement.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="times-container__item" v-for="date in dates">{{date}}</div>
+</template>
+
+<script>
+
+export default {
+  name: "TimeElement.vue",
+  props: ['dates']
+}
+</script>
+
+<style scoped>
+.times-container__item {
+  padding: 10px;
+  margin-bottom: 10px;
+  background: #fffdb1;
+  border: 1px solid #e1dfa2;
+  border-radius: 4px;
+}
+</style>
--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeKiller.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeKiller.vue
@@ -1,16 +1,34 @@
 <template>
     <div class="time-killer">
-        <!--Начало-->
-
-        <!--Конец-->
+      <button type="button" class="btn btn-warning"@click="addDate">Добавить время!</button>
+       <div class="time-container">
+         <TimeElement :dates = 'dates' @mouseover="remove"> </TimeElement>
+       </div>
     </div>
 </template>
 
 <script>
+import TimeElement from "./TimeElement.vue";
+
 export default {
     name: 'TimeKiller',
-    // Начало
+  components: {TimeElement},
+   data(){
+      return {
+        dates:[]
+      }
+      },
 
+  // Начало
+methods: {
+      addDate(){
+      let nowDate = new Date();
+      this.dates.push(nowDate);
+      },
+  remove(){
+        this.dates.remove();
+  }
+}
     // Конец
 };
 </script>
