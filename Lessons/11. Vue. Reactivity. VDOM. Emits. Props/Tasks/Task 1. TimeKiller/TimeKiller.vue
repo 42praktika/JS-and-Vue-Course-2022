dc8b8d6e14490076e855695ac151563ed302e50c
Oksana.Maiatina <okmay0625@gmail.com>
2022-11-28 23:10:49 +0300

    + hw for lesson 11

--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeKiller.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeKiller.vue
@@ -1,16 +1,40 @@
 <template>
     <div class="time-killer">
         <!--Начало-->
-
+        <button type="button" class="btn btn-warning" @click="addTime">
+            Добавить время!
+        </button>
+        <div class="time-container">
+            <TimeElement :dates="dates" @mouseover="removeTime"></TimeElement>
+        </div>
         <!--Конец-->
     </div>
 </template>
 
 <script>
+import TimeElement from './TimeElement.vue';
+
 export default {
     name: 'TimeKiller',
     // Начало
+    components: { 
+        TimeElement, 
+    },
+    data() {
+        return {
+            dates:[],
+        };
+    },
 
+    methods: {
+        addTime() {
+            let currentDate = new Date();
+            this.dates.push(currentDate);
+        },
+        removeTime(index){
+            this.dates.splice(index, 1);
+        },
+    }
     // Конец
 };
 </script>
