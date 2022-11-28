5280ae3b21d90b689615f2005ff1ed83fd0cc3e4
KseniaKatargina <90481483+KseniaKatargina@users.noreply.github.com>
2022-11-28 19:44:40 +0300

    done task from lesson 11

---
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeElement.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="times-container">
+    <div
+        v-for="(item,index) in items"
+        @mouseover="$emit('remove', index)"
+        class="times-container__item"
+    >
+    {{item}}
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "TimeElement",
+    props: {
+            items: {
+            type: Array,
+            default: () => [],
+        },
+    },
+}
+</script>
+
+<style scoped>
+.times-container__item {
+    padding: 10px;
+    margin-bottom: 10px;
+    background: #fffdb1;
+    border: 1px solid #e1dfa2;
+    border-radius: 4px;
+}
+</style>
--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeKiller.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeKiller.vue
@@ -1,16 +1,43 @@
 <template>
     <div class="time-killer">
         <!--Начало-->
-
+        <button
+            @click="addItem"
+            type="button"
+            class="btn btn-warning"
+        >
+            Добавить время!
+        </button>
+        <TimeElement :items="items" @remove="removeItem"></TimeElement>
         <!--Конец-->
     </div>
 </template>
 
 <script>
+import TimeElement from './TimeElement.vue';
+
 export default {
     name: 'TimeKiller',
     // Начало
 
+    components: {
+            TimeElement,
+        },
+        data() {
+            return {
+                items:[],
+            };
+        },
+        methods: {
+            addItem() {
+                const currentDate = new Date();
+                this.items.push(currentDate);
+            },
+            removeItem(index){
+                this.items.splice(index, 1);
+            }
+
+        }
     // Конец
 };
 </script>
