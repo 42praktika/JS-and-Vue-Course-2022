303f6077273f25015bbc65aae61d37ebfa373369
Dmitriy Alexandrov <diman2190@gmail.com>
2022-12-05 11:58:05 +0300

    HeadHunter fixes

---
+++ vue/App/HeadHunterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+import HeadHunter
+    from '../../Lessons/14. Vuex/Tasks/Task 1. HeadHunter/HeadHunter';
+
+export default {
+    name: 'HeadHunterComponent',
+    components: { HeadHunter },
+};
+</script>
