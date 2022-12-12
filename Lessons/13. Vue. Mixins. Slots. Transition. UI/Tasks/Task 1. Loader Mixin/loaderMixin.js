c2f1147ecb603221c8ac0e78faf1bf15b89a39bd
DariaGazkaeva <d.gazkaeva@gmail.com>
2022-12-12 05:54:16 +0000

    done lesson13.task and lesson13.homework

--- Lessons/13. Vue. Mixins. Slots. Transition. UI/Tasks/Task 1. Loader Mixin/loaderMixin.js
+++ Lessons/13. Vue. Mixins. Slots. Transition. UI/Tasks/Task 1. Loader Mixin/loaderMixin.js
@@ -1,6 +1,20 @@
 const loaderMixin = {
     // Начало
-
+    methods: {
+        setLoading(isLoading) {
+            if (isLoading) {
+                const loader = document.createElement('div');
+                loader.className = 'loader';
+                loader.textContent = 'Загрузка...';
+                this.$el.append(loader);
+            } else {
+                const elem = this.$el.querySelector('.loader');
+                if (elem != null) {
+                    elem.remove();
+                }
+            }
+        },
+    },
     // Конец
 };
 
