488f2190a5cf1bdbecaab2f35dec47cea61630ce
Oksana.Maiatina <okmay0625@gmail.com>
2022-12-12 20:53:44 +0300

    + all for lesson 13

--- Lessons/13. Vue. Mixins. Slots. Transition. UI/Homework/Task 1. Cinema/components/DirectorCard.vue
+++ Lessons/13. Vue. Mixins. Slots. Transition. UI/Homework/Task 1. Cinema/components/DirectorCard.vue
@@ -1,6 +1,38 @@
 <template>
-    <!--Начало-->
+    <!--Начало--> 
+    <CardBlock>
+        <template v-slot:image>
+            <img :src="director.image">
+        </template>
+        
+        <template v-slot:info>
+            <div class="director__info-name">
+                {{ director.name }}
+            </div>
 
+            <div>
+                <span class="director__title">
+                    Возраст:
+                </span>
+                {{ director.age }}
+            </div>
+
+            <div>
+                <span class="director__title">
+                    Место рождения:
+                </span>
+                {{ director.birthPlace }}
+            </div>
+
+            <div>
+                <span class="director__title">
+                    Фильмы:
+                </span>
+                {{ director.movies }}
+            </div>
+
+        </template>
+    </CardBlock>
     <!--Конец-->
 </template>
 
@@ -11,7 +43,18 @@ export default {
     name: 'DirectorCard',
     components: { CardBlock },
     // Начало
+    props: {
+        director: {
+            id: Number,
+            name: String,
+            image: String,
+            age: Number,
+            birthPlace: String,
+            movies: String,
+        },
 
+        default: () => {},
+    }
     // Конец
 };
 </script>
