1197f0a94144c8ca1205c677f7f4830fd1fb65e5
Oksana.Maiatina <okmay0625@gmail.com>
2022-12-12 20:55:26 +0300

    + all for lesson 13

--- Lessons/13. Vue. Mixins. Slots. Transition. UI/Tasks/Task 1. Loader Mixin/loaderMixin.js
+++ Lessons/13. Vue. Mixins. Slots. Transition. UI/Tasks/Task 1. Loader Mixin/loaderMixin.js
@@ -1,6 +1,24 @@
 const loaderMixin = {
-    // Начало
+    // Начало (у меня где-то заруинилась сборка, и поэтому в текущем репозитории vue компоненты нельзя нормально отследить :'))
+    methods: {
+        setLoading(isLoading) {
+            if (isLoading) {
+                if (document.querySelector('.loader') === null) {
+                    const loader = document.createElement('div');
+                    loader.className = 'loader';
+                    loader.innerHTML = 'Загрузка...';
+                    this.$el.append(loader);
+                }
+            }
+            else {
+                const loaderEl = this.$el.querySelector('.loader');
 
+                if (loaderEl != null) {
+                    loaderEl.remove();
+                }
+            }
+        }
+    }
     // Конец
 };
 
