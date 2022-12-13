0077f606eac56dcdff61a95a337e895b1639eb1e
Igtisamova.Regina <reginaigtisamova@gmail.com>
2022-12-13 12:49:06 +0300

    Задачи 13 урокаÐÐ

--- Lessons/13. Vue. Mixins. Slots. Transition. UI/Tasks/Task 1. Loader Mixin/loaderMixin.js
+++ Lessons/13. Vue. Mixins. Slots. Transition. UI/Tasks/Task 1. Loader Mixin/loaderMixin.js
@@ -1,7 +1,22 @@
 const loaderMixin = {
-    // Начало
-
-    // Конец
+    methods: {
+        setLoading(isLoaded) {
+            if (isLoaded) {
+                if (!document.querySelector('.loader')) {
+                    const divLoaderElement = document.createElement('div')
+                    divLoaderElement.className = 'loader';
+                    divLoaderElement.textContent = 'Загрузка...';
+                    this.$el.append(divLoaderElement)
+                }
+            }
+            else {
+                const loaderElement = this.$el.querySelector('.loader');
+                if (loaderElement != null) {
+                    loaderElement.remove();
+                }
+            }
+        }
+    }
 };
 
 export default loaderMixin;
