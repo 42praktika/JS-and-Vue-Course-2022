4e802f37499fb14f9b1e63ac5aa22c88f2c3c3c3
askarik <askarsaliahov@gmail.com>
2022-12-13 09:52:54 +0300

    Homework 13

--- Lessons/13. Vue. Mixins. Slots. Transition. UI/Homework/Task 1. Cinema/components/DirectorCard.vue
+++ Lessons/13. Vue. Mixins. Slots. Transition. UI/Homework/Task 1. Cinema/components/DirectorCard.vue
@@ -1,29 +1,52 @@
 <template>
-    <!--Начало-->
-
-    <!--Конец-->
+  <card-block>
+    <template v-slot:image>
+      <img :src="director.image">
+    </template>
+    <template v-slot:info>
+      <div class="director__info-name">
+        {{ director.name }}
+      </div>
+      <div>
+        <span class="director__title">
+          Возраст:
+        </span>
+        {{ director.age }}
+      </div>
+      <div>
+        <span class="director__title">
+          Место рождения:
+        </span>
+        {{ director.birthPlace }}
+      </div>
+      <div>
+        <span class="director__title">
+          Фильмы:
+        </span>
+        {{ director.movies }}
+      </div>
+    </template>
+  </card-block>
 </template>
 
 <script>
 import CardBlock from './CardBlock.vue';
 
 export default {
-    name: 'DirectorCard',
-    components: { CardBlock },
-    // Начало
-
-    // Конец
+  name: 'DirectorCard',
+  components: { CardBlock },
+  props:['director']
 };
 </script>
 
 <style>
 .director__info-name {
-    font-size: 18px;
-    font-weight: bold;
+  font-size: 18px;
+  font-weight: bold;
 }
 
 .director__title {
-    font-size: 16px;
-    font-weight: bold;
+  font-size: 16px;
+  font-weight: bold;
 }
 </style>
--- Lessons/13. Vue. Mixins. Slots. Transition. UI/Homework/Task 1. Cinema/components/MovieCard.vue
+++ Lessons/13. Vue. Mixins. Slots. Transition. UI/Homework/Task 1. Cinema/components/MovieCard.vue
@@ -1,29 +1,52 @@
 <template>
-    <!--Начало-->
-
-    <!--Конец-->
+  <card-block>
+    <template v-slot:image>
+      <img :src="movie.image">
+    </template>
+    <template v-slot:info>
+      <div class="movie__info-name">
+        {{ movie.name }}
+      </div>
+      <div>
+        <span class="movie__title">
+          Год:
+        </span>
+        {{ movie.year }}
+      </div>
+      <div>
+        <span class="movie__title">
+          Режиссер:
+        </span>
+        {{ movie.director }}
+      </div>
+      <div>
+        <span class="movie__title">
+          Жанр:
+        </span>
+        {{ movie.genre }}
+      </div>
+    </template>
+  </card-block>
 </template>
 
 <script>
 import CardBlock from './CardBlock.vue';
 
 export default {
-    name: 'MovieCard',
-    components: { CardBlock },
-    // Начало
-
-    // Конец
+  name: 'MovieCard',
+  components: { CardBlock },
+  props:['movie']
 };
 </script>
 
 <style>
 .movie__info-name {
-    font-size: 18px;
-    font-weight: bold;
+  font-size: 18px;
+  font-weight: bold;
 }
 
 .movie__title {
-    font-size: 16px;
-    font-weight: bold;
+  font-size: 16px;
+  font-weight: bold;
 }
 </style>
